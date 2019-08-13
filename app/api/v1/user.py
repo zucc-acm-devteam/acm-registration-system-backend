00340fa07db8775b794cfd0791e1604e6b93e01c
taoting <335354289@qq.com>
2019-08-13 11:39:31 +0800

    修复bug

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -3,6 +3,7 @@ from flask import jsonify, g
 from app.libs.error_code import CreateSuccess, NotFound, AuthFailed, Success
 from app.libs.redprint import Redprint
 from app.libs.token_auth import auth
+from app.models.mail import Mail
 from app.models.user import User
 from app.validators.forms import RegisterForm, CodeForm
 
@@ -42,11 +43,11 @@ def create_user_api():
     return CreateSuccess('register successful')
 
 
-@api.route('activation', methods=['POST'])
+@api.route('/activation', methods=['POST'])
 @auth.login_required
 def activate_user_api():
     form = CodeForm().validate_for_api()
-    if not User.check_code(g.user.username, form.code.data):
+    if not Mail.check_code(g.user.username, form.code.data):
         raise AuthFailed('code is incorrect')
     User.modify(g.user.username, permission=1)
     return Success('activate success')
