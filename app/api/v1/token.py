23f0ff0ac9b662b7893fec95a1a94457b3b32c23
taoting <335354289@qq.com>
2019-08-14 10:56:07 +0800

    优化token路由

--- app/api/v1/token.py
+++ app/api/v1/token.py
@@ -1,11 +1,25 @@
-from flask import current_app, jsonify
+from flask import current_app, jsonify, g
 
+from app.libs.error_code import DeleteSuccess
 from app.libs.redprint import Redprint
+from app.libs.token_auth import auth
 from app.models.user import User
 from app.validators.forms import LoginForm
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 api = Redprint('token')
+
+
+@api.route('', methods=['GET'])
+@auth.login_required
+def get_token_info_api():
+    user = g.user
+    return jsonify({
+        'code': 0,
+        'data': {
+            'user': user
+        }
+    })
 
 
 @api.route('', methods=['POST'])
@@ -20,6 +34,26 @@ def get_token_api():
             'token': token.decode('ascii')
         }
     }), 201
+
+
+@api.route('', methods=['PUT'])
+@auth.login_required
+def update_token_api():
+    identity = {'uid': g.user.username}
+    expiration = current_app.config['TOKEN_EXPIRATION']
+    token = _generate_auth_token(identity['uid'], expiration)
+    return jsonify({
+        'code': 0,
+        'data': {
+            'token': token.decode('ascii')
+        }
+    }), 201
+
+
+@api.route('', methods=['DELETE'])
+@auth.login_required
+def delete_token_api():
+    return DeleteSuccess()
 
 
 def _generate_auth_token(uid, expiration):
